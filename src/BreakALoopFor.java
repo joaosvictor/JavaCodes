774f35864e6b733701a09b979a95ee5da412fe70
Joao Victor <48931503+joaovictorsantos@users.noreply.github.com>
2020-07-03 10:55:12 -0300

    Update BreakALoopFor.java

--- src/BreakALoopFor.java
+++ src/BreakALoopFor.java
@@ -2,10 +2,10 @@ public class MyClass {
   public static void main(String[] args) {
     for (int i = 0; i < 10; i++) {
       if (i == 4) {
-        break;
+        break;//instead of continue the for loop, it will break if find the result.
       }
       System.out.println(i);
     }  
   }
 }
-//
+
