df226ed4443a95131fc9cca1646eb32a63be4cbd
Joao Victor <48931503+joaovictorsantos@users.noreply.github.com>
2020-07-03 10:55:46 -0300

    Update SimpleArray.java

--- src/SimpleArray.java
+++ src/SimpleArray.java
@@ -1,6 +1,6 @@
 public class MyClass {
   public static void main(String[] args) {
     String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-    System.out.println(cars[0]);
+    System.out.println(cars[0]);//-> Volvo
   }
 }
